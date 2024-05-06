912e988a5123d7a06d71935857345efb4b16cb09
ameen <ameensahil1323@gmail.com>
2024-05-06 10:00:00 +0530

    Initial commit

---
+++ leapyear.js
@@ -0,0 +1,12 @@
+let year=2020;
+if(year%4==0)
+{
+    if(year%100!=0)
+        console.log("leap year");
+    else{
+        console.log("not a leap year");
+    }
+}
+else{
+    console.log("not leap year");
+}
