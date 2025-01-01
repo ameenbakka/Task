e3115f632f26489dc3fe0844c4688a4986ac0374
ameen <ameensahil1323@gmail.com>
2025-01-01 10:00:00 +0530

    positive

---
+++ patttern1.js
@@ -0,0 +1,4 @@
+let str = "string";
+for (let i = 1; i <= str.length; i++) {
+    console.log(str.slice(0, i));
+}
---
+++ positive.js
@@ -0,0 +1,7 @@
+let num=-1;
+if(num>0)
+    console.log("the number is positive");
+else if(num<0)
+    console.log("The number is negative");
+else
+console.log("the number is zero");
