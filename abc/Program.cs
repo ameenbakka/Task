b7f0cbe307f74f14d5816177d0281d72d0df2341
ameen <ameensahil1323@gmail.com>
2025-03-24 14:46:38 +0530

    abc commit

---
+++ abc/Program.cs
@@ -0,0 +1,29 @@
+﻿    namespace abc
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+               Fruit apple = new Fruit("red", "apple");
+            apple.Display();
+           
+            }
+        }
+        public class Fruit
+        {
+            public string Color;
+            public string Name;
+            public Fruit( string FruitColor, string FruitName)
+            {
+                Color = FruitColor;
+                Name = FruitName;
+            }
+            public void Display()
+            {
+                Console.WriteLine(Name);
+                Console.WriteLine(Color);
+            }
+
+       
+        }
+    }
